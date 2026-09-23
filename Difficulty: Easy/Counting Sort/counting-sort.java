15e87f1515ab83f43bd128f376a3bf22271d8cda
Ashish Kumar Gupta <ashishsantosh543@gmail.com>
2026-09-23 12:27:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Counting Sort/counting-sort.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static String countSort(String s) {
+        // code here\
+        int []freq=new int[26];
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<26;i++){
+            while(freq[i]>0){
+                sb.append((char)(i+'a'));
+                freq[i]--;
+            }
+        }
+        return sb.toString();
+        
+        
+    }
+}
